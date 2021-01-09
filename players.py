54e9c74787d4db8786b1a51e78d532fbe5406ef2
Graham Hoyes <accounts@grahamhoyes.com>
2021-01-09 12:15:09 -0500

    Add players file

---
+++ players.py
@@ -0,0 +1,16 @@
+import random
+import numpy as np
+from typing import List, Dict, Optional, Tuple
+import copy
+
+from site_location import (
+    SiteLocationPlayer,
+    Store,
+    SiteLocationMap,
+    euclidian_distances,
+    attractiveness_allocation,
+)
+
+
+class FooPlayer(SiteLocationPlayer):
+    pass
